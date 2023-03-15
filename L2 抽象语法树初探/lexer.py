962cbc857b7c4363d4e2a05a1facb41233a799d7
insorker <2677331029@qq.com>
2023-03-15 20:06:42 +0800

    nothing

---
+++ L2 抽象语法树初探/lexer.py
@@ -0,0 +1,69 @@
+from enum import Enum, auto
+import re
+
+class TokenType(Enum):
+  NUMBER = auto()
+# FIXME:
+  IDENTIFIER = auto()
+  STRING = auto()
+  BINARY_OPERATER = auto()
+  EQUALS = auto()
+  OPEN_PAREN = auto()
+  CLOSE_PAREN = auto()
+  SPACE = auto()
+  EOF = auto()
+
+TOKEN_REGEX = {
+# FIXME:
+  TokenType.NUMBER: r'[1-9]+[0-9]*|0',
+# FIXME:
+  TokenType.IDENTIFIER: r'[a-zA-Z_]+[a-zA-Z_0-9]*',
+  TokenType.STRING: r'".*?"',
+  TokenType.BINARY_OPERATER: r'[+\-*/%]',
+  TokenType.EQUALS: r'=',
+  TokenType.OPEN_PAREN: r'\(',
+  TokenType.CLOSE_PAREN: r'\)',
+  TokenType.SPACE: r'[ \t\r]',
+}
+
+class Token:
+  def __init__(self, type: TokenType, value: str) -> None:
+    self.type: TokenType = type
+    self.value: str = value
+  
+  def __repr__(self):
+    return f'{{ {self.type}, {self.value} }}'
+
+def tokenize(code: str) -> list[Token]:
+  tokens: list[Token] = []
+  match = None
+
+  while code:
+    for tokentype, pattern in TOKEN_REGEX.items():
+      match = re.match(pattern, code)
+
+      if match:
+        value = match.group()
+        code = code[len(value):]
+        # FIXME:
+        if tokentype == TokenType.SPACE:
+          continue
+        elif tokentype == TokenType.STRING:
+          tokens.append(Token(tokentype, value[1:-1]))
+        else:
+          tokens.append(Token(tokentype, value))
+        break
+
+    if match == None:
+      raise Exception(__file__, 'Not valid charater: ', code[0])
+
+  tokens.append(Token(TokenType.EOF, 'EOF'))
+  return tokens
+
+
+print('Repl v1.0:')
+while True:
+  code = input('>>> ')
+  tokens = tokenize(code)
+  for token in tokens:
+    print(token)
