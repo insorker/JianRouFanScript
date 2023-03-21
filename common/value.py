6686d088022d80eafaa6973fc4c1d7aca20fd639
insorker <2677331029@qq.com>
2023-03-21 20:12:25 +0800

    finish function call 🎉🎉🎉 cheers

--- common/value.py
+++ common/value.py
@@ -16,9 +16,13 @@ class Value:
   
 
 class Function(Value):
-  def __init__(self, block) -> None:
+  def __init__(self, name: str, params: list[str], block, ar) -> None:
     super().__init__()
+    self.value = name
+    self.name = name
+    self.params = params
     self.block = block
+    self.ar = ar
 
 
 class Integer(Value):
